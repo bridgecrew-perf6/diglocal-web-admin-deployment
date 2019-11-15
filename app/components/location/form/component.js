104a0b895bcc759b76905aaecae311a2dbb75d0f
Julia Donaldson <julia.donaldson@gmail.com>
2019-11-15 01:51:35 -0800

    Tag inputs, etc

--- app/components/location/form/component.js
+++ app/components/location/form/component.js
@@ -1,9 +1,36 @@
 import Component from '@ember/component';
+import { not } from '@ember/object/computed';
 
 export default Component.extend({
+  isEditing: false,
+  isReadonly: not('isEditing'),
+
+  classNames: [ 'border rounded p-4' ],
+
+  classNameBindings: [ 'isEditing:bg-gray-100' ],
+
+  rollbackModel() {
+    if (this.model && this.model.get('hasDirtyAttributes')) {
+      this.model.rollbackAttributes();
+    }
+  },
+
+  willDestroyElement() {
+    this.rollbackModel();
+    this._super(...arguments);
+  },
+
   actions: {
-    save(model) {
-      model.save();
+    save() {
+      this.model.save();
+    },
+    cancel() {
+      this.rollbackModel();
+      this.set('isEditing', false);
+    },
+    delete() {
+      this.model.deleteRecord();
+      this.model.save();
     }
   }
 });
