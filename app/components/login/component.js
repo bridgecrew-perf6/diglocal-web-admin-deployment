cb6f97b1b5f20d123ab4c1b21cadb8a7410e7b33
Julia Donaldson <julia.donaldson@gmail.com>
2019-11-16 13:54:19 -0800

    update login

--- app/components/login/component.js
+++ app/components/login/component.js
@@ -1,6 +1,8 @@
 import Component from '@ember/component';
-import { getProperties } from '@ember/object';
+import { inject as service } from '@ember/service';
+import { get, getProperties, set } from '@ember/object';
 import { alias } from '@ember/object/computed';
+import { task } from 'ember-concurrency';
 import { validator, buildValidations } from 'ember-cp-validations';
 
 const Validations = buildValidations({
@@ -16,11 +18,42 @@ const Validations = buildValidations({
 
 
 export default Component.extend(Validations, {
-  onLogin() {},
-  onSignUp() {},
+  session: service('session'),
+  firebaseApp: service(),
+  store: service(),
+
+  isLogin: true,
+  authenticating: false,
+  error: null,
 
   username: '',
   password: '',
+
+  authenticateWithEmail: task(function* (email, password) {
+    set(this, 'authenticating', true);
+    const auth = yield get(this, 'firebaseApp').auth();
+    try {
+      let authenticatedUser = yield auth.signInWithEmailAndPassword(email, password);
+      return authenticatedUser;
+    } catch(e) {
+      // console.log(e);
+      set(this, 'error', e);
+    }
+    set(this, 'authenticating', false);
+  }),
+
+  signupWithEmail: task(function* (email, password) {
+    set(this, 'authenticating', true);
+    const auth = yield get(this, 'firebaseApp').auth();
+    try {
+      let createdUser = yield auth.createUserWithEmailAndPassword(email, password);
+      return createdUser;
+    } catch(e) {
+      // console.log(e);
+      set(this, 'error', e);
+    }
+    set(this, 'authenticating', false);
+  }),
 
   isFormValid: alias('validations.isValid'),
 
@@ -29,8 +62,14 @@ export default Component.extend(Validations, {
       if (this.isFormValid)  {
         let { username, password } = getProperties(this, 'username', 'password');
 
-        this.isLogin ? this.onLogin(username, password) : this.onSignUp(username, password);
+        this.isLogin ?
+          this.authenticateWithEmail.perform(username, password) :
+          this.signupWithEmail.perform(username, password);
       }
+    },
+    switchActionType() {
+      set(this, 'isLogin', !this.isLogin);
+      set(this, 'errors', null);
     }
   }
 });
