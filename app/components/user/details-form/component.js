8cafd2a7a4a57dbae7a4264526aca9dfa1532cb2
Cory Loken <cory@me.com>
2019-11-17 12:32:42 -0500

    Update user pages with new format

---
+++ app/components/user/details-form/component.js
@@ -0,0 +1,49 @@
+import Component from '@ember/component';
+import { set } from '@ember/object';
+import { not } from '@ember/object/computed';
+import { task } from 'ember-concurrency';
+import  { inject as service } from '@ember/service';
+
+export default Component.extend({
+  store: service(),
+
+  isEditing: false,
+  isReadonly: not('isEditing'),
+
+  classNames: [ 'border rounded p-4' ],
+
+  classNameBindings: [ 'isEditing:bg-gray-100' ],
+
+  showDestroyModal:  false,
+
+  rollbackModel() {
+    if (this.model && this.model.get('hasDirtyAttributes')) {
+      this.model.rollbackAttributes();
+    }
+  },
+
+  willDestroyElement() {
+    this.rollbackModel();
+    this.setProperties({
+      showDestroyModal: false,
+      isEditing: false
+    });
+    this._super(...arguments);
+  },
+
+  actions: {
+    save() {
+      this.model.save();
+      this.set('isEditing', false);
+    },
+    cancel() {
+      this.rollbackModel();
+      this.set('isEditing', false);
+    },
+    delete() {
+      this.model.deleteRecord();
+      this.model.save();
+      this.router.transitionTo('authenticated.users');
+    },
+  },
+});
