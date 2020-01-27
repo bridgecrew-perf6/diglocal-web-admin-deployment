d3855bf97f2ad28b95c52fe831906e1475a0e5b5
Cory Loken <cory@me.com>
2020-01-27 14:49:52 -0500

    Allow scoop active to be edited

--- app/models/scoop.js
+++ app/models/scoop.js
@@ -1,18 +1,18 @@
 import Model, { attr, belongsTo } from '@ember-data/model';
 
 export default Model.extend({
-  description: attr(),
-  fineText: attr(),
-  imageThumb: attr(),
-  eventDate: attr(),
-  eventStartTime: attr(),
-  eventEndTime: attr(),
-  isRecurring: attr(),
-  ticketUrl: attr(),
-  paidRank: attr(),
   active: attr(),
-  postAt: attr(),
+  description: attr(),
+  eventDate: attr(),
+  eventEndTime: attr(),
+  eventStartTime: attr(),
+  fineText: attr(),
   image: attr(),
+  imageThumb: attr(),
+  isRecurring: attr(),
+  paidRank: attr(),
+  postAt: attr(),
+  ticketUrl: attr(),
 
   /*************************
   **  Relationships       **
