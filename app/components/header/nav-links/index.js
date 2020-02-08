244f1acc6b59bfcfb5ae57b1a56a52925e11d00a
Julia Donaldson <julia.donaldson@gmail.com>
2020-02-07 16:45:07 -0800

    refactor: Componentize nav links

---
+++ app/components/header/nav-links/index.js
@@ -0,0 +1,8 @@
+import { inject as service } from '@ember/service';
+import Component from '@glimmer/component';
+
+export default class Header extends Component {
+  @service regions;
+  @service currentUser;
+
+}
