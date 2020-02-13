9255cda0c7a9d952e24d66e626f16b1a3db7151b
Julia Donaldson <julia.donaldson@gmail.com>
2020-02-13 12:56:35 -0800

    test: business index switch region

--- tests/acceptance/businesses/businesses-index-test.js
+++ tests/acceptance/businesses/businesses-index-test.js
@@ -1,5 +1,6 @@
 import { module, test } from 'qunit';
-import { visit, currentURL } from '@ember/test-helpers';
+import { visit, currentURL, click } from '@ember/test-helpers';
+import { testId } from 'diglocal-manage/tests/helpers';
 import { authenticateSession } from 'ember-simple-auth/test-support';
 import setupAdminUserTest from 'diglocal-manage/tests/helpers/setup-admin-user-test';
 import setupActiveRegion from 'diglocal-manage/tests/helpers/setup-active-region';
@@ -10,9 +11,12 @@ module('Acceptance | Businesses | Index', function(hooks) {
 
   hooks.beforeEach(function() {
     let region = this.activeRegion;
+    let otherRegion = this.server.create('region');
     this.server.createList('business', 5, { region });
+    this.server.createList('business', 3, { region: otherRegion });
 
     this.region = region;
+    this.otherRegion = otherRegion;
     this.url = `/region/${this.region.id}/businesses`;
   });
 
@@ -21,7 +25,21 @@ module('Acceptance | Businesses | Index', function(hooks) {
     await visit(this.url);
 
     assert.equal(currentURL(), this.url);
-    assert.dom('[data-test-id=business-list-row]').exists({ count: 5 });
+    assert.dom(testId('business-listing')).exists({ count: 5 });
+  });
+
+  test('I can switch my active region and see all businesses updated', async function(assert) {
+    await authenticateSession();
+    await visit(this.url);
+
+    assert.equal(currentURL(), this.url);
+    assert.dom(testId('business-listing')).exists({ count: 5 });
+
+    await click(testId('select-region-dd-trigger'));
+    await click(`${testId('select-region', this.otherRegion.id)} button`);
+
+    assert.equal(currentURL(), `/region/${this.otherRegion.id}/businesses`);
+    assert.dom(testId('business-listing')).exists({ count: 3 });
   });
 
 });
