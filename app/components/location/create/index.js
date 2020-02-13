6e23e26ce30252cffaa0925c63b5204097badcc0
Julia Donaldson <julia.donaldson@gmail.com>
2020-02-12 21:13:05 -0800

    chore: remove needless commented-out code

--- app/components/location/create/index.js
+++ app/components/location/create/index.js
@@ -18,17 +18,6 @@ export default class Create extends Component {
       business: this.args.business
     });
 
-    // for (let i = 0; i < 7; i++) {
-    //   let operatingHour = this.store.createRecord('operatingHour', {
-    //     dayOfWeek: i,
-    //     location,
-    //     openTime: '09:00:00',
-    //     closeTime: '20:00:00',
-    //     closed: false
-    //   });
-    //   location.operatingHours.pushObject(operatingHour);
-    // }
-
     this.newRecord = location;
   }
 }
