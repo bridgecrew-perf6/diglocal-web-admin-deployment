1644cfdb2592e5bc55b1eb986dd940e1c60ddd4f
Julia Donaldson <julia.donaldson@gmail.com>
2020-02-20 00:23:47 -0800

    refactor: category listing and create form

--- app/routes/authenticated/region/site-settings.js
+++ app/routes/authenticated/region/site-settings.js
@@ -1,7 +1,22 @@
 import Route from '@ember/routing/route';
+import { tracked } from '@glimmer/tracking';
+import { inject as service } from '@ember/service';
 
 export default class AuthenticatedRegionSiteSettingsRoute extends Route {
-  breadCrumb = {
-    title: 'Settings'
-  };
+  @service regions;
+  @tracked breadCrumb;
+
+  model() {
+    return this.regions.activeRegion;
+  }
+
+  afterModel(model) {
+    let modelName = model.longName;
+
+    if (modelName) {
+      this.breadCrumb = {
+        title: modelName
+      };
+    }
+  }
 }
