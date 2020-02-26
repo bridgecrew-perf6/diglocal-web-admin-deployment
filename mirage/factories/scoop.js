26824c35b6e2c80cfe705a037145646d0ff9ceb9
Julia Donaldson <julia.donaldson@gmail.com>
2020-02-25 17:34:43 -0800

    test: view scoop

--- mirage/factories/scoop.js
+++ mirage/factories/scoop.js
@@ -4,16 +4,28 @@ import faker from 'faker';
 
 export default Factory.extend({
   isRecurring: false,
+  isDeal: false,
   active: true,
   description(i) { return `${faker.commerce.productAdjective()} Local Scoop ${i + 1}` },
   fineText() { return faker.lorem.sentences() },
   eventDate() { return moment(faker.date.future()).format('YYYY-MM-DD') },
-  eventStartTime: '17:00:00',
-  eventEndTime: '19:00:00',
-  ticketUrl: 'https://www.ticketmaster.com',
+  postAt() { return moment().format('YYYY-MM-DDTHH:mm:ss.SSSSZ') },
 
-  // TODO - what does a recurring scoop look like exactly?
   recurring: trait({
     isRecurring: true,
+    daysOfWeek: [0,6], // Sunday, Saturday
+    recurringDisplayFrom: '2018-10-23',
+    recurringDisplayTo:  '2050-10-23',
+    postAt: null,
+    eventStartTime: null,
+    eventEndTime: null,
+    ticketUrl: null
   }),
+
+  eventWithTicketsAndTimes: trait({
+    eventStartTime: '17:00:00',
+    eventEndTime: '19:00:00',
+    ticketUrl: 'https://www.ticketmaster.com'
+  })
+
 });
---
+++ tests/acceptance/businesses/view/edit-scoop-test.js
@@ -0,0 +1,46 @@
+import { module, test } from 'qunit';
+import { visit, currentURL, click, find } from '@ember/test-helpers';
+import { authenticateSession } from 'ember-simple-auth/test-support';
+import { testId } from 'diglocal-manage/tests/helpers';
+import setupAdminUserTest from 'diglocal-manage/tests/helpers/setup-admin-user-test';
+import setupActiveRegion from 'diglocal-manage/tests/helpers/setup-active-region';
+
+module('Acceptance | View Business | Scoops | Edit', function(hooks) {
+  setupAdminUserTest(hooks);
+  setupActiveRegion(hooks);
+
+  hooks.beforeEach(function() {
+    let region = this.activeRegion;
+    this.business = this.server.create('business', 'withLocation', { region });
+    this.region = region;
+  });
+
+  module('View scoop', function(hooks) {
+    hooks.beforeEach(function() {
+      this.scoop = this.server.create('scoop', {
+        business: this.business,
+        description: 'Sample Sale',
+        fineText: 'Prices up to 80% off on all items',
+        isDeal: true
+      });
+      this.url = `/region/${this.region.id}/businesses/${this.business.id}/scoops/${this.scoop.id}`;
+    });
+
+    test('I can view scoop fields as readonly and then click to toggle edit', async function(assert) {
+      await authenticateSession();
+      await visit(this.url);
+
+      assert.equal(currentURL(), this.url);
+
+      let detailsForm = find(testId('scoop-details-form'));
+      assert.dom(detailsForm).exists();
+      assert.dom(detailsForm).hasAttribute('data-test-editing', 'false');
+
+      await click(testId('edit-scoop'));
+      assert.dom(detailsForm).hasAttribute('data-test-editing', 'true');
+      assert.dom(testId('cancel-edit')).exists();
+      assert.dom(testId('save-scoop')).exists();
+    });
+  });
+
+});
