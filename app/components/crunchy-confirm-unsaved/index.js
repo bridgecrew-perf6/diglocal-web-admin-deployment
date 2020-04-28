d38f5f20d6c2b817d53d6ddc97890fb6f88b10b7
Julia Donaldson <julia.donaldson@gmail.com>
2020-04-27 18:24:21 -0700

    Rename component and update to accept arrays Add to location templates to monitor changes for all locations on page and avoid multiple modals

--- app/components/crunchy-confirm-unsaved/index.js
+++ app/components/crunchy-confirm-unsaved/index.js
@@ -2,6 +2,7 @@ import Component from '@glimmer/component';
 import { inject as service } from '@ember/service';
 import { action } from '@ember/object';
 import { tracked } from '@glimmer/tracking';
+import { isPresent }  from '@ember/utils';
 
 export default class ConfirmUnsavedComponent extends Component {
   @service router;
@@ -10,12 +11,22 @@ export default class ConfirmUnsavedComponent extends Component {
 
   previousTransition = null;
 
-  get isDirtyModel() {
-    let model = this.args.model;
+  checkDirty(model) {
     if (model.hasDirtyAttributes) {
       return true;
     }
     return false;
+  }
+
+  checkAnyDirty(models) {
+    let dirtyModels = models.toArray().filter(this.checkDirty);
+    return dirtyModels.length > 0;
+  }
+
+  get hasUnsavedAttributes() {
+    let models = this.args.models;
+    let model = this.args.model;
+    return isPresent(models) ? this.checkAnyDirty(models) : this.checkDirty(model);
   }
 
   @action
@@ -23,7 +34,7 @@ export default class ConfirmUnsavedComponent extends Component {
     let { to: toRouteInfo, from: fromRouteInfo } = transition;
 
     if (toRouteInfo.name !== fromRouteInfo.name) {
-      if (this.isDirtyModel) {
+      if (this.hasUnsavedAttributes) {
         this.previousTransition = transition;
         transition.abort();
         this.showUnsavedAlert = true;
