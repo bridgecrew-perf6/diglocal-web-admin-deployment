7b0dbcc53e6eed62ae266a58d3f8564d6ef5770a
Julia Donaldson <julia.donaldson@gmail.com>
2020-06-19 16:19:42 -0700

    Update newsletter management: search, view, delete

--- app/controllers/authenticated/region/newsletters/index.js
+++ app/controllers/authenticated/region/newsletters/index.js
@@ -1,13 +1,17 @@
 import config from 'diglocal-manage/config/environment';
 import Controller from '@ember/controller';
 import { oneWay } from '@ember/object/computed';
-import { action } from '@ember/object';
+import { action, get } from '@ember/object';
 import { task, timeout } from 'ember-concurrency';
 import { tracked } from '@glimmer/tracking';
+import { inject as service } from '@ember/service';
 
 const INPUT_DEBOUNCE = config.environment !== 'test' ? 250 : 0;
 
 export default class AuthenticatedRegionsNewslettersIndexController extends Controller {
+  @service store;
+  @service router;
+
   queryParams = [
     'search',
     'sort'
@@ -17,7 +21,7 @@ export default class AuthenticatedRegionsNewslettersIndexController extends Cont
     email: 'Sort by email',
     fname: 'Sort by first name',
     lname: 'Sort by last name',
-    created_at: 'Sort by date created',
+    created_at: 'Sort by date subscribed',
   };
 
   @tracked search = '';
@@ -49,6 +53,16 @@ export default class AuthenticatedRegionsNewslettersIndexController extends Cont
   @action
   removeFilter(filter, arrayName) {
     this[arrayName].removeObject(filter);
+  }
+
+  @action
+  async removeSubscriber(subscriber) {
+    // subscriber is a proxy, so we must use 'get'
+    let id = get(subscriber, 'id');
+    
+    let newsletter = this.store.peekRecord('newsletter', id);
+    await newsletter.destroyRecord();
+    await this.router.transitionTo('authenticated.region.newsletters');
   }
 }
 
