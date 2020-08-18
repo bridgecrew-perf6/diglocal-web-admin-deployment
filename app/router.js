6cb738f152ae694703db4fac0adbd8cc77a8aa1b
Cory Loken <cory@me.com>
2020-08-18 19:43:38 -0400

    Update to reflect ember requirements

--- app/router.js
+++ app/router.js
@@ -1,9 +1,7 @@
-import classic from 'ember-classic-decorator';
 import EmberRouter from '@ember/routing/router';
 import config from './config/environment';
 
-@classic
-class Router extends EmberRouter {
+export default class Router extends EmberRouter {
   location = config.locationType;
   rootURL = config.rootURL;
 }
@@ -92,5 +90,3 @@ Router.map(function() {
 
   this.route('manage', function() {});
 });
-
-export default Router;
