7b325bc31e166782fea6e24e8c8c1d94a065ec7c
Cory Loken <cory@me.com>
2020-08-20 16:28:00 -0400

    Query for data

--- app/routes/authenticated/region/businesses/view/homes/view.js
+++ app/routes/authenticated/region/businesses/view/homes/view.js
@@ -1,3 +1,7 @@
 import Route from '@ember/routing/route';
 
-export default class AuthenticatedRegionBusinessesViewHomesViewRoute extends Route {}
+export default class AuthenticatedRegionBusinessesViewHomesViewRoute extends Route {
+  model(params) {
+    return this.store.findRecord('home', params.home_id, { include: 'location,location.business,digitalAssets,avatar' } );
+  }
+}
